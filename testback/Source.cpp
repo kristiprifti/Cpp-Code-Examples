a486b77cb9ef73fa54fd54c113abc3de084bff23
Kristi Prifti <kristiprifti59@gmail.com>
2022-07-11 12:42:46 -0400

    string using the swap function

--- testback/Source.cpp
+++ testback/Source.cpp
@@ -4,33 +4,24 @@ using namespace std;
 
 int main () {
 
-    string unformatted_full_name{ "StephenHawking" };
-    string formatted_full_name{ };
-    string first_name{};
-    string last_name{};
+    string journal_entry_1{ "Isaac Newton" };
+    string journal_entry_2{ "leibniz" };
 
-    //----DO NOT MODIFY THE CODE ABOVE THIS LINE----
-    //----WRITE YOUR CODE BELOW THIS LINE----
-    first_name = unformatted_full_name.substr(0, 7);
-    last_name = unformatted_full_name.substr(7, unformatted_full_name.size());
-    formatted_full_name = first_name + " " + last_name;
+    ////----DO NOT MODIFY THE CODE ABOVE THIS LINE----
+    ////----WRITE YOUR CODE BELOW THIS LINE----
+    journal_entry_1.erase(0,6);
+    if (journal_entry_1 < journal_entry_2) {
+        journal_entry_1.swap(journal_entry_2);
+    }
+    else {
+        journal_entry_2.swap(journal_entry_1);
+    }
 
-    //----WRITE YOUR CODE ABOVE THIS LINE----
-    //----DO NOT MODIFY THE CODE BELOW THIS LINE----
 
-    cout << formatted_full_name;
+    ////----WRITE YOUR CODE ABOVE THIS LINE----
+    ////----DO NOT MODIFY THE CODE BELOW THIS LINE----
 
-    //His solution 
-    //string first_name{ unformatted_full_name, 0, 7 }; // intesrting what is this 
-    // 
-    //     string s6 {s1, 0, 3};   // App
-         //string s7(10, 'X');    // XXXXXXXXXX
-    // 
-    //string last_name = unformatted_full_name.substr(7, 7);
-
-    //string formatted_full_name = first_name + last_name;
-
-    //formatted_full_name.insert(7, " ");
+    cout << journal_entry_1 << "\n" << journal_entry_2;
 
 
     return 0;
